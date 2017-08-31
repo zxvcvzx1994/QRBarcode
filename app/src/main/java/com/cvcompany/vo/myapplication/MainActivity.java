e76143cee8a4f949cb7917ef9521831370fac5dc
zxvcvzx1994 <zxvcvzx1994@gmail.com>
2017-08-31 14:16:47 +0700

    Initial commit

---
+++ app/src/main/java/com/cvcompany/vo/myapplication/MainActivity.java
@@ -0,0 +1,62 @@
+package com.cvcompany.vo.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.txtAddress)
+    TextView txtAddress;
+    @BindView(R.id.txtName)
+    TextView txtName;
+    @BindView(R.id.img)
+    ImageView img;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn)
+    public void btn(){
+        new IntentIntegrator(this).initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Picasso.with(this).load(result.getContents()).into(img);
+                try {
+                    JSONObject jsonObject = new JSONObject(result.getContents());
+                    txtAddress.setText(""+jsonObject.getString("address"));
+                    txtName.setText(""+jsonObject.getString("name"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
